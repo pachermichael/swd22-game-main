adb31f7128919731077bf53d9d98d1893c8329c3
Michael Pacher <s52103994@edu.campus02.at>
2022-04-30 10:46:44 +0200

    Initial commit

---
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/Gras.java
@@ -0,0 +1,32 @@
+package at.compus02.swd.ss2022.game.gameobjects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Gras implements GameObject{
+    private Texture image;
+    private Sprite sprite;
+
+    public Gras() {
+        image = new Texture("tile_gras.png");
+        sprite = new Sprite(image);
+        sprite.setScale(16);
+    }
+    @Override
+    public void act(float delta) {
+
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        sprite.setPosition(x, y);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        sprite.draw(batch);
+    }
+}
+
+
